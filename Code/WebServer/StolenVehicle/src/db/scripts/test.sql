a8108073f3818ff4f8ba2c181958ca24eea5ed9b
Jithin Mohanan <jitsonfire@gmail.com>
2016-08-01 22:41:11 +0530

    updated code

--- Code/WebServer/StolenVehicle/src/db/scripts/test.sql
+++ Code/WebServer/StolenVehicle/src/db/scripts/test.sql
@@ -15,3 +15,6 @@ select * from vehicle;
 
 update theft_information
 set status = 'LOST';
+
+
+select ti.id,u.name,u.id, v.registrationNo,ti.theft_dateTime,ti.theft_location_cordinates,v.id,v.type,v.make,v.model,v.year_of_make from theft_information ti,vehicle v,user u where ti.status = 'lost' and ti.vehicle_id = v.id and v.user_id = u.id and ti.id = '8f06c43d-0947-4e6d-914a-607c1492eb8f';
--- Code/WebServer/StolenVehicle/src/main/java/com/stolenvehicle/constants/Query.java
+++ Code/WebServer/StolenVehicle/src/main/java/com/stolenvehicle/constants/Query.java
@@ -10,7 +10,7 @@ public class Query {
 	public static final String SET_USER_PASSWORD = "update user set status = 'ACTIVE',activation_id = null,password = ? where emailaddress = ? and activation_id=?";
 	public static final String SAVE_VEHICLE = "insert into vehicle values(?,?,?,?,?,?,?,?,?,?,?,?,now(),null,null);";
 	public static final String SAVE_THEFT = "insert into theft_information values(?,?,?,?,?,?,?,?,?,now(),null,null);";
-	public static final String GET_THEFT_INFO_BY_ID = "select ti.id,u.name,u.id, v.registrationNo,ti.theft_dateTime,ti.theft_location_cordinates,v.id,v.type,v.make,v.model,v.year_of_make from theft_information ti,vehicle v,user u where ti.status = 'lost' and ti.vehicle_id = v.id and v.user_id = u.id and ti.id = ?";
+	public static final String GET_THEFT_INFO_BY_ID = "select ti.id,ti.status,u.name,u.id, v.registrationNo,ti.theft_dateTime,ti.theft_location_cordinates,v.id,v.type,v.make,v.model,v.year_of_make from theft_information ti,vehicle v,user u where ti.status = 'lost' and ti.vehicle_id = v.id and v.user_id = u.id and ti.id = ?";
 	public static final String GET_VEHICLE_ATTACHMENTS = "select * from attachment where vehicle_id = ?";
 	public static final String GET_THEFT_INFO_BY_REGISTRATION_NUMBER = "select ti.id,ti.status,u.id,u.name, v.id,v.registrationNo,ti.theft_dateTime,ti.theft_location_cordinates,v.type,v.make,v.model,v.year_of_make from theft_information ti,vehicle v,user u where ti.status = 'lost' and ti.vehicle_id = v.id and v.user_id = u.id and v.registrationNo  like ?";
 	public static final String UPDATE_THEFT_INFO_STATUS = "update theft_information set status = ?,find_information_id = ? where id = ?;";
